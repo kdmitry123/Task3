6bd2cbf98ef0732bdfed8cdbf00df3f61a654eab
kdmitry123 <60309700+kdmitry123@users.noreply.github.com>
2020-08-05 14:47:10 +0300

    Update TestAppTest.java

--- src/test/java/org/example/TestAppTest.java
+++ src/test/java/org/example/TestAppTest.java
@@ -20,7 +20,7 @@ public class TestAppTest {
     @Test
     public void testTwo(){
         System.out.println("\nRussia -------------------------------------------------------\n");
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(FakerImpl.generateInfo(Objects.requireNonNull(FakerImpl.getInstance("ru_RU"))));
         }
         System.out.println();
@@ -29,7 +29,7 @@ public class TestAppTest {
     @Test
     public void testThree(){
         System.out.println("\nUSA -------------------------------------------------------\n");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(FakerImpl.generateInfo(Objects.requireNonNull(FakerImpl.getInstance("en_US"))));
         }
         System.out.println();
